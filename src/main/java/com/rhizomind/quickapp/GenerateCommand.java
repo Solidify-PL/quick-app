cfec40fd2da8006b53113b7ab83165da79fdea60
Tomasz Krzyżak <tomasz.krzyzak@solidify.pl>
2025-07-09 19:01:22 +0200

    feature: add 'package' command

---
+++ src/main/java/com/rhizomind/quickapp/GenerateCommand.java
@@ -0,0 +1,137 @@
+package com.rhizomind.quickapp;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+/**
+ * quick-app --values=values.yaml -value param1=value1 --template=./spring --output=./output
+ * --force
+ */
+@Command(
+        name = "generate",
+        mixinStandardHelpOptions = true,
+        description = "Generates source code based on a template"
+)
+class GenerateCommand implements Callable<Integer> {
+
+    @Option(names = {"-v", "--values"}, description = "Values yaml ")
+    private File valuesFile;
+
+    @Option(names = {"--value"},
+            description = "Parametry nadpisujace wartosci z pliku values --value <nazwa>=<wartość>",
+            paramLabel = "<nazwa>=<wartość>")
+    private Map<String, String> values = new HashMap<>();
+
+    @Option(names = {"-t", "--template"}, description = "template directory")
+    private File templateDir;
+
+    @Option(names = {"-o",
+            "--output"}, description = "Output directory (Workdir by default) ", required = true)
+    private File outputDir;
+
+    @Option(names = {"-f", "--force"}, description = "Rewrite output", defaultValue = "false")
+    private boolean forceRewrite;
+
+    @Override
+    public Integer call() throws Exception {
+        if (outputDir.exists()) {
+            if (!outputDir.isDirectory()) {
+                throw new IllegalArgumentException(
+                        "Output directory '" + outputDir.getAbsolutePath()
+                                + "' is not a directory");
+            }
+        } else {
+            outputDir.mkdirs();
+        }
+
+        var templateFilesDir = new File(templateDir, "files");
+        var manifest = Commons.loadManifest(new FileInputStream(new File(templateDir, "manifest.yaml")));
+        var values = Commons.loadMapParameters(this.valuesFile);
+        var defaults = Commons.loadMapParameters(new File(templateDir, manifest.getValues().getDefaults()));
+        var merge = merge(this.values, merge(values, defaults));
+
+        var tempValuesFile = Files.createTempFile("quickapp", "values").toFile();
+
+        Commons.OBJECT_MAPPER.writeValue(tempValuesFile, merge);
+        System.out.println("Using following values:");
+        Commons.OBJECT_MAPPER.writeValue(System.out, merge);
+        System.out.println();
+        System.out.flush();
+
+        var command = "docker run --rm -u $(id -u):$(id -g) "
+                + " -v " + templateFilesDir.getAbsolutePath() + ":/tmp/quickapp/input:ro "
+                + " -v " + tempValuesFile.getAbsolutePath()
+                + ":/tmp/quickapp/values.yaml:ro "
+                + " -v " + outputDir.getAbsolutePath() + ":/tmp/quickapp/output "
+                + " " + manifest.getEngine().getImage()
+                + " --input=/tmp/quickapp/input "
+                + " --values=/tmp/quickapp/values.yaml "
+                + " --output=/tmp/quickapp/output "
+                + (forceRewrite ? " --force " : "")
+                + join(manifest.getEngine().getArgs());
+
+        return execute(command);
+    }
+
+    private String join(List<String> args) {
+        StringBuilder builder = new StringBuilder();
+        for (String arg : args) {
+            builder.append(arg).append(" ");
+        }
+        return builder.toString();
+    }
+
+    private Integer execute(String command) throws InterruptedException, IOException {
+        System.out.println("Executing command: " + command);
+        ProcessBuilder builder = new ProcessBuilder("/bin/sh", "-c", command);
+        Process process = builder.start();
+
+        // stdout
+        Thread out = new Thread(() -> {
+            try (BufferedReader r = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()))) {
+                r.lines().forEach(System.out::println);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // stderr
+        Thread err = new Thread(() -> {
+            try (BufferedReader r = new BufferedReader(
+                    new InputStreamReader(process.getErrorStream()))) {
+                r.lines().forEach(System.err::println);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        out.start();
+        err.start();
+
+        int code = process.waitFor();
+        out.join();
+        err.join();
+        return code;
+    }
+
+    private Map<String, String> merge(Map<String, String> values, Map<String, String> defaults) {
+        HashMap<String, String> result = new HashMap<>();
+        result.putAll(defaults);
+        result.putAll(values);
+        return result;
+    }
+
+
+}
---
+++ src/main/java/com/rhizomind/quickapp/PackageCommand.java
@@ -0,0 +1,168 @@
+package com.rhizomind.quickapp;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import picocli.CommandLine;
+
+import java.io.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+        name = "package",
+        mixinStandardHelpOptions = true,
+        description = "Packages template directory into tar.gz file"
+)
+public class PackageCommand implements Callable<Integer> {
+
+    @CommandLine.Option(names = {"-t",
+            "--template"}, description = "template directory (Workdir by default)")
+    private File templateDir;
+
+    @CommandLine.Option(names = {"-o",
+            "--output"}, description = "Output directory (Workdir by default) ")
+    private File outputDir;
+
+    @Override
+    public Integer call() throws Exception {
+        var templateDir = this.templateDir == null ? new File(".") : this.templateDir;
+        var outputDir = this.outputDir == null ? new File(".") : this.outputDir;
+
+        if (isValidTemplateDir(templateDir)) {
+            Manifest manifest = Commons.loadManifest(new File(templateDir, "manifest.yaml"));
+            File targetFile = new File(outputDir,
+                    manifest.getName() + "-" + manifest.getVersion() + ".tar.gz");
+            System.out.println("Packaging template  into " + targetFile + " file...");
+
+            createTarGz(templateDir, manifest.getName(), targetFile);
+
+            System.out.println("Template packaged into " + targetFile + " file successfully.");
+            return 0;
+        }
+        throw new RuntimeException("Invalid template directory: " + templateDir.getAbsolutePath());
+    }
+
+    private boolean isValidTemplateDir(File templateDir) {
+        return templateDir.exists()
+                && templateDir.isDirectory()
+                && new File(templateDir, "manifest.yaml").exists()
+                && new File(templateDir, "parameters-defaults.yaml").exists()
+                && new File(templateDir, "parameters-schema.json").exists()
+                && new File(templateDir, "files").exists()
+                && new File(templateDir, "files").isDirectory()
+                && validateManifestFile(new File(templateDir, "manifest.yaml"))
+                && validateSchemaFile(new File(templateDir, "parameters-schema.json"))
+                && validateDefaultsFile(
+                new File(templateDir, "parameters-schema.json"),
+                new File(templateDir, "parameters-defaults.yaml"));
+    }
+
+    private boolean validateDefaultsFile(File schemaFile, File defaultsFile) {
+        try {
+            JsonNode valuesNode = Commons.OBJECT_MAPPER.readTree(defaultsFile);
+            JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(
+                    SpecVersion.VersionFlag.V7);
+            JsonSchema schema = schemaFactory.getSchema(new FileInputStream(schemaFile));
+
+            var validationMessages = schema.validate(valuesNode);
+            if (!validationMessages.isEmpty()) {
+                System.out.println(
+                        defaultsFile + " is not compatible with schema " + validationMessages);
+            }
+            return validationMessages.isEmpty();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean validateSchemaFile(File schemaFile) {
+        try {
+            JsonNode schemaNode = Commons.OBJECT_MAPPER.readTree(schemaFile);
+            JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(
+                    SpecVersion.VersionFlag.V7);
+            JsonSchema metaSchema = schemaFactory.getSchema(
+                    JsonSchemaFactory.class.getResourceAsStream("/draft-07/schema"));
+            var validationMessages = metaSchema.validate(schemaNode);
+            if (!validationMessages.isEmpty()) {
+                System.out.println(
+                        schemaFile + " is a proper JsonSchema file " + validationMessages);
+            }
+            return validationMessages.isEmpty();
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    schemaFile.getAbsolutePath() + " is not correct JSONSchema file", e);
+        }
+    }
+
+    private boolean validateManifestFile(File manifestFile) {
+        try {
+            Commons.loadManifest(manifestFile);
+        } catch (IOException e) {
+            System.out.println(manifestFile + " is not a valid manifest file " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static void createTarGz(File templateDir, String templateName, File dstFile)
+            throws IOException {
+        // Tworzenie pliku tar.gz
+        try (FileOutputStream fos = new FileOutputStream(dstFile);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                GzipCompressorOutputStream gzos = new GzipCompressorOutputStream(bos);
+                TarArchiveOutputStream tarOut = new TarArchiveOutputStream(gzos)) {
+            tarOut.setLongFileMode(TarArchiveOutputStream.LONGFILE_POSIX);
+
+            var templateDirPath = templateDir.toPath();
+            Files.walkFileTree(templateDirPath, new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                        throws IOException {
+                    // Tworzenie ścieżki względnej względem sourceDir
+                    Path relativePath = templateDirPath.relativize(file);
+                    // Dodanie templateName jako prefiksu w archiwum
+                    String entryName =
+                            templateName + "/" + relativePath.toString().replace("\\", "/");
+
+                    // Tworzenie wpisu TAR
+                    TarArchiveEntry entry = new TarArchiveEntry(file.toFile(), entryName);
+                    tarOut.putArchiveEntry(entry);
+
+                    // Zapisanie zawartości pliku do archiwum
+                    Files.copy(file, tarOut);
+                    tarOut.closeArchiveEntry();
+
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                        throws IOException {
+                    // Tworzenie ścieżki względnej i dodanie templateName
+                    Path relativePath = templateDirPath.relativize(dir);
+                    String entryName = relativePath.toString().isEmpty()
+                            ? templateName + "/"
+                            : templateName + "/" + relativePath.toString().replace("\\", "/") + "/";
+
+                    // Tworzenie wpisu dla katalogu
+                    TarArchiveEntry entry = new TarArchiveEntry(entryName);
+                    tarOut.putArchiveEntry(entry);
+                    tarOut.closeArchiveEntry();
+
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            tarOut.finish();
+        }
+    }
+
+}
